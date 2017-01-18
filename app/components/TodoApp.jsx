dcd2d4c7823ee665e0b32b7cb9e0a1588f902ba2
Ana Priscila Alves <pva@cardio-id.com>
2017-01-18 17:07:24 +0000

    Refactor TodoList, Todo, and AddTodo to work with redux

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,27 +2,23 @@ var React = require("react");
 var uuid = require("node-uuid");
 var moment = require("moment");
 
-var TodoList = require("TodoList");
-var AddTodo = require("AddTodo");
+import TodoList from 'TodoList';
+import AddTodo from 'AddTodo';
+
 var TodoSearch = require("TodoSearch");
 var TodoAPI = require('TodoAPI');
 
 var TodoApp = React.createClass({
-    getInitialState: function () {
-        return {
-            showCompleted: false,
-            searchText: "",
-            todos: TodoAPI.getTodos()
-        };
+    getInitialState: function() {
+        return {showCompleted: false, searchText: "", todos: TodoAPI.getTodos()};
     },
-    componentDidUpdate: function () {
+    componentDidUpdate: function() {
         TodoAPI.setTodos(this.state.todos);
     },
-    handleAddTodo: function (text) {
+    handleAddTodo: function(text) {
         this.setState({
             todos: [
-                ...this.state.todos,
-                {
+                ...this.state.todos, {
                     id: uuid(),
                     text: text,
                     completed: false,
@@ -33,23 +29,11 @@ var TodoApp = React.createClass({
             ]
         });
     },
-    handleToggle: function (id) {
-        var updatedTodos = this.state.todos.map(function (todo) {
-            if (todo.id === id) {
-                todo.completed = !todo.completed
-                todo.completedAt = todo.completed ? moment().unix() : undefined
-            }
-            return todo;
-        });
-        this.setState({todos:updatedTodos});
+
+    handleSearch: function(showCompleted, searchText) {
+        this.setState({showCompleted: showCompleted, searchText: searchText.toLowerCase()});
     },
-    handleSearch: function (showCompleted, searchText) {
-        this.setState({
-            showCompleted: showCompleted,
-            searchText: searchText.toLowerCase()
-        });
-    },
-    render: function () {
+    render: function() {
         var {todos, showCompleted, searchText} = this.state;
         var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
         return (
@@ -58,9 +42,9 @@ var TodoApp = React.createClass({
                 <div className="row">
                     <div className="column small-centered small-11 medium-6 large-5">
                         <div className="container">
-                            <TodoSearch onSearch={this.handleSearch} />
-                            <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
-                            <AddTodo onAddTodo={this.handleAddTodo} />
+                            <TodoSearch onSearch={this.handleSearch}/>
+                            <TodoList />
+                            <AddTodo onAddTodo={this.handleAddTodo}/>
                         </div>
                     </div>
                 </div>
